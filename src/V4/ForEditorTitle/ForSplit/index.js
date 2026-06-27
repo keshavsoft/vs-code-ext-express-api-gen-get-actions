4d3c29123e59640ee4220e9bdde4118b2d7ef523
keshavsoft <nknnkumar@yahoo.com>
2026-06-27 11:52:15 +0530

    4 st

---
+++ src/V4/ForEditorTitle/ForSplit/index.js
@@ -0,0 +1,41 @@
+import * as vscode from 'vscode';
+import fs from 'fs';
+import path from 'path';
+
+import ShowAll from "../../CommonCommands/ShowAll/v2/orchestration/startOrchestration.js";
+import Find from "../../CommonCommands/Find/v1/orchestration/startOrchestration.js";
+import filterQuery from "../../CommonCommands/FilterQuery/v1/orchestration/startOrchestration.js";
+
+const splitEditor = async (context, uri) => {
+    const panel = vscode.window.createWebviewPanel(
+        "endpointUi",
+        "Endpoint UI",
+        vscode.ViewColumn.Beside,
+        {
+            enableScripts: true
+        }
+    );
+
+    panel.webview.onDidReceiveMessage(
+        (message) => {
+            if (message.action === "showAll") {
+                ShowAll({ uri });
+            };
+
+            if (message.action === "find") {
+                Find({ uri });
+            };
+
+            if (message.action === "filterQuery") {
+                filterQuery({ uri });
+            };
+        }
+    );
+
+    panel.webview.html = fs.readFileSync(
+        path.join(import.meta.dirname, "ui", "index.html"),
+        "utf8"
+    );
+};
+
+export default splitEditor;
